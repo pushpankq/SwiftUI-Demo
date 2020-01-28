5443e90f3cbda18ec888a7507a26b6e62368bbf3
Pushpank <pushpankq@gmail.com>
2020-01-28 23:34:49 +0530

    Add files via upload
    Grid + Chunks Array

---
+++ Hiking/Grid-Demo/Grid-Demo/ContentView.swift
@@ -0,0 +1,45 @@
+
+
+import SwiftUI
+
+struct ContentView : View {
+    
+    let dishes = Dish.all()
+    
+    var body: some View {
+        
+        let chunkedDishes = dishes.chunked(into: 3)
+        
+        return List {
+            
+            // rows
+            ForEach(0..<chunkedDishes.count) { index in
+                
+                HStack {
+                    
+                    ForEach(chunkedDishes[index]) { dish in
+                        
+                        Image(dish.imageURL)
+                            .resizable()
+                           // .frame(width: 150, height: 150)
+                            .scaledToFit()
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
